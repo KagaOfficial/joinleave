8471b03f2e5dddc935d73f19b07799f75e1ebec3
KagaOfficial <121429179+KagaOfficial@users.noreply.github.com>
2022-12-31 18:44:55 +0100

    Create joinleave.java

---
+++ joinleave.java
@@ -0,0 +1,11 @@
+package me.;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Joinleave extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        // Plugin startup logic
+    }
+}
